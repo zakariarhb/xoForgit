184eb152e9f1d4f28eff5dbc99e90d03ea0a2143
zakariarhb <zakariarhb@gmail.com>
2016-05-27 22:32:42 +0100

    Modification des valeur des valeurs des name lablels.

--- ApplicationXO/Form1.Designer.cs
+++ ApplicationXO/Form1.Designer.cs
@@ -46,9 +46,9 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
+            this.x_win_count = new System.Windows.Forms.Label();
+            this.game_over_count = new System.Windows.Forms.Label();
+            this.o_win_count = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -239,47 +239,47 @@
             this.label3.TabIndex = 12;
             this.label3.Text = "Victoire de O";
             // 
-            // label4
+            // x_win_count
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(35, 290);
-            this.label4.Margin = new System.Windows.Forms.Padding(3);
-            this.label4.Name = "label4";
-            this.label4.Padding = new System.Windows.Forms.Padding(5);
-            this.label4.Size = new System.Drawing.Size(23, 23);
-            this.label4.TabIndex = 13;
-            this.label4.Text = "0";
+            this.x_win_count.AutoSize = true;
+            this.x_win_count.Location = new System.Drawing.Point(31, 293);
+            this.x_win_count.Margin = new System.Windows.Forms.Padding(3);
+            this.x_win_count.Name = "x_win_count";
+            this.x_win_count.Padding = new System.Windows.Forms.Padding(5);
+            this.x_win_count.Size = new System.Drawing.Size(23, 23);
+            this.x_win_count.TabIndex = 13;
+            this.x_win_count.Text = "0";
             // 
-            // label5
+            // game_over_count
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(119, 290);
-            this.label5.Margin = new System.Windows.Forms.Padding(3);
-            this.label5.Name = "label5";
-            this.label5.Padding = new System.Windows.Forms.Padding(5);
-            this.label5.Size = new System.Drawing.Size(23, 23);
-            this.label5.TabIndex = 14;
-            this.label5.Text = "0";
+            this.game_over_count.AutoSize = true;
+            this.game_over_count.Location = new System.Drawing.Point(115, 293);
+            this.game_over_count.Margin = new System.Windows.Forms.Padding(3);
+            this.game_over_count.Name = "game_over_count";
+            this.game_over_count.Padding = new System.Windows.Forms.Padding(5);
+            this.game_over_count.Size = new System.Drawing.Size(23, 23);
+            this.game_over_count.TabIndex = 14;
+            this.game_over_count.Text = "0";
             // 
-            // label6
+            // o_win_count
             // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(203, 290);
-            this.label6.Margin = new System.Windows.Forms.Padding(3);
-            this.label6.Name = "label6";
-            this.label6.Padding = new System.Windows.Forms.Padding(5);
-            this.label6.Size = new System.Drawing.Size(23, 23);
-            this.label6.TabIndex = 15;
-            this.label6.Text = "0";
+            this.o_win_count.AutoSize = true;
+            this.o_win_count.Location = new System.Drawing.Point(199, 293);
+            this.o_win_count.Margin = new System.Windows.Forms.Padding(3);
+            this.o_win_count.Name = "o_win_count";
+            this.o_win_count.Padding = new System.Windows.Forms.Padding(5);
+            this.o_win_count.Size = new System.Drawing.Size(23, 23);
+            this.o_win_count.TabIndex = 15;
+            this.o_win_count.Text = "0";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(262, 322);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
+            this.Controls.Add(this.o_win_count);
+            this.Controls.Add(this.game_over_count);
+            this.Controls.Add(this.x_win_count);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -326,9 +326,9 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label x_win_count;
+        private System.Windows.Forms.Label game_over_count;
+        private System.Windows.Forms.Label o_win_count;
     }
 }
 
